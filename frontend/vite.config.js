b24fb23de3cb5663a279c080f943851897e62edc
Z687870 <philisophiliso524@gmail.com>
2024-11-01 12:45:29 +0200

    new chages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ECOMMERCE-WEBSITE',
   plugins: [react()],
   server: {port:5173}
 })
